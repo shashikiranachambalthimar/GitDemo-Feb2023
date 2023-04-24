1a034e63cd844d4dcd41b90b11ce3aa3e9ebbf89
shashikirana <shashikiranadeepak@gmail.com>
2023-04-24 11:54:16 +0100

    Jira1111- committing prjet for first time

---
+++ src/main/java/uk/axone/devintest/git/Application.java
@@ -0,0 +1,7 @@
+package uk.axone.devintest.git;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Git is cool");
+    }
+}
---
+++ src/test/java/uk/axone/devintest/git/tests/App_test.java
@@ -0,0 +1,12 @@
+package uk.axone.devintest.git.tests;
+
+import org.junit.Test;
+
+public class App_test {
+    @Test
+
+    public void test1(){
+        System.out.println("Running test 1");
+    }
+
+}
